0f0d53ef432caca99ddc0a0b447a1a6764e8a1d5
Daniel Yanisse <daniel.yanisse@mogreet.com>
2012-06-09 15:58:51 -0700

    better management of iframes for vimeo/youtube

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Content.create( :user_id => '1', :url => 'http://www.youtube.com/embed/u1zgFlCw8Aw', :rate_up => 0, :rate_down =>  0)
-Content.create( :user_id => '1', :url => 'http://www.youtube.com/embed/A8IVASo0umU', :rate_up => 0, :rate_down =>  0)
+Content.create( :user_id => '1', :url => 'http://www.youtube.com/embed/u1zgFlCw8Aw', :rate_up => 0, :rate_down =>  0, :source => 'youtube')
+Content.create( :user_id => '1', :url => 'http://www.youtube.com/embed/A8IVASo0umU', :rate_up => 0, :rate_down =>  0, :source => 'youtube')
+Content.create( :user_id => '1', :url => 'http://player.vimeo.com/video/43708825', :rate_up => 0, :rate_down =>  0, :source => 'vimeo')
 
