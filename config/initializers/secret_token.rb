50814f906a7c473e10289f8fbb19cef87f7af984
Ludovic Henry <ludovichenry.utbm@gmail.com>
2012-06-09 09:50:13 -0700

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoList::Application.config.secret_token = '545bd7118edac5f3b7caca1f05467de93acefa47525c67ed1ef820714f940d114a6f5a40c2303894d92b16ace62970249ca3a7c177f5edd58453304656320c7d'
