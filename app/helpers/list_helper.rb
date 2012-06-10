adbe9a85d75aedaea376b69949e315fd3b43b132
Ludovic Henry <ludovichenry.utbm@gmail.com>
2012-06-10 13:16:25 -0700

    Cache fb friends ids

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -1,35 +1,39 @@
 module ListHelper
   def self.get_facebook_friends_ids access_token
-    fb_friend_ids = []
-    fb_url        = "https://graph.facebook.com/me/friends?access_token=#{access_token}"
+    ids = Rails.cache.fetch('FB-FRIENDS-' + access_token) do
+      fb_friend_ids = []
+      fb_url        = "https://graph.facebook.com/me/friends?access_token=#{access_token}"
 
-    begin
       begin
-        response = RestClient.get(fb_url)
-      rescue RestClient::BadRequest => e
-        raise "FacebookAPI error: #{e.inspect}"
-      rescue => e
-        raise "FacebookAPI error: #{e.message}"
-      end
+        begin
+          response = RestClient.get(fb_url)
+        rescue RestClient::BadRequest => e
+          raise "FacebookAPI error: #{e.inspect}"
+        rescue => e
+          raise "FacebookAPI error: #{e.message}"
+        end
 
-      if response == ''
-        raise 'FacebookAPI error: received empty response'
-      end
+        if response == ''
+          raise 'FacebookAPI error: received empty response'
+        end
 
-      response_hash = JSON.parse(response)
+        response_hash = JSON.parse(response)
 
-      if response_hash.has_key? 'error'
-        raise "FacebookAPI error : #{response_hash['error']['message']}"
-      end
+        if response_hash.has_key? 'error'
+          raise "FacebookAPI error : #{response_hash['error']['message']}"
+        end
 
-      response_hash["data"].each do |fb_friend|
-        fb_friend_ids << (Integer(fb_friend['id']) rescue nil)
-      end
+        response_hash["data"].each do |fb_friend|
+          fb_friend_ids << (Integer(fb_friend['id']) rescue nil)
+        end
 
-      fb_url = response_hash['paging']['next']
-    end while not response_hash.nil? and not response_hash['data'].nil? and response_hash['data'].size > 0
+        fb_url = response_hash['paging']['next']
+      end while not response_hash.nil? and not response_hash['data'].nil? and response_hash['data'].size > 0
 
-    return fb_friend_ids
+      fb_friend_ids
+    end
+
+    return ids
   rescue => e
     return []
   end
