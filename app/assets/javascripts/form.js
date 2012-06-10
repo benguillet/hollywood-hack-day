df5f3bf9b55b97536e8df36d1f811760cb4da4dd
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-06-10 13:26:10 -0700

    add loader

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -4,29 +4,38 @@ $(document).ready(function() {
     var url = $('#url').text();
 
     $('#add_list').click(function() {
+        var loading_add_list = $('#loading_add_list');
+        loading_add_list.show();
         $.ajax({
           type: "POST",
           url: "/share-to/me",  
           data: {'url': url},
           success: function() {
-            $('#notification').show();
+            loading_add_list.hide();
+            $('#notification').text('success').show();
           },
           error: function() {
-
+            loading_add_list.hide();
+            $('#notification').text('fail').show();
           }
         });
     });
 
     $('#share_friends').click(function() {
         var url = $('#url').text();
+        var loading_share_friends = $('#loading_share_friends');
+        loading_share_friends.show();
         $.ajax({
           type: "POST",
           url: "/share-to/friends",  
           data: {'url': url},
-          success: function() {  
-           $('#notification').show();
+          success: function() {
+            loading_share_friends.hide();
+            $('#notification').text('success').show();
           },
           error: function() {
+            loading_share_friends.hide();
+            $('#notification').text('fail').show();
             
           }
         });  
