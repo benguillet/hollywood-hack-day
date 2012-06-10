f19a992cb6ecc4160338e5095add103bebcef8a7
Thomas <pukoune@Toms-MacBook-Pro.local>
2012-06-10 16:11:47 -0700

    fix sorting

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -10,7 +10,7 @@ class ListController < ApplicationController
       query = Content.where(:user_id => current_user.id, :access => 'me')
     end
 
-    @videos = query.order('post_date DESC').limit(5)
+    @videos = query.order('strftime(\'%j\', post_date) DESC, rate_up-(rate_down*1.5) DESC').limit(5)
 
     if @videos.empty? and (params[:before] or params[:after])
       redirect_to '/me'
@@ -38,7 +38,7 @@ class ListController < ApplicationController
       query = Content.where(:user_id => @users.map { |e| e.id }, :access => 'friends')
     end
 
-    @videos = query.includes(:user).order('post_date DESC').limit(5)
+    @videos = query.includes(:user).order('strftime(\'%j\', post_date) DESC, rate_up-(rate_down*1.5) DESC').limit(5)
 
     if @videos.empty? and (params[:before] or params[:after])
       redirect_to '/friends'
