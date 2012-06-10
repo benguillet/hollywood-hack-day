c23c1bd08539ce6384fdc3d6030f0bd480efc112
Thomas <pukoune@Toms-MacBook-Pro.local>
2012-06-10 00:41:36 -0700

    fix host from youtube links

--- app/controllers/import_fb_controller.rb
+++ app/controllers/import_fb_controller.rb
@@ -69,7 +69,6 @@ class ImportFbController < ApplicationController
   
   #
   def import_user_and_friends_shared_videos
-    reset_session
     import_user_latest_shared_video_links('me', USER_VIDEO_FQL_LIMIT)
     #import_friends_latest_shared_video_links
   end
@@ -83,8 +82,7 @@ class ImportFbController < ApplicationController
     content.user_id   = current_user.id
     content.url       = ShareHelper::sanitize_url(url)
     content.post_date = Time.now.strftime('%Y-%m-%d %H:%M:%S')
-    content.source    = url.match(/www\.(.*)\.com/)[1]
-    
+    content.source    = URI(content.url).host.match(/www\.(.*)\.com/)[1]
     content.access    = 'friends'
 
     content.save
