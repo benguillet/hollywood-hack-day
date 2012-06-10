632381f3a71a4ab95c4d030c89c9f6c66e94d09f
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-06-10 01:26:22 -0700

    heroku compliant bitch

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,54 +1,32 @@
 //= require jquery
 $(document).ready(function() {
+    var url = $('#url').text();
+
     $('#add_list').click(function() {
-        var url = $('#url').text();
         console.log(url);
         $.ajax({
           type: "POST",
-          url: "http://localhost.com:3000/share-to/me",  
+          url: "/share-to/me",  
           data: {'url': url},
-          success: function() {  
-           
-          }  
-        });  
+          success: function() {
+
+          }
+        });
     });
 
     $('#share_friends').click(function() {
         var url = $('#url').text();
         console.log(url);
         $.ajax({
-          type: "POST",
-          url: "http://localhost:3000/share-to/friends",  
-          data: {'url': url},
-          success: function() {  
-           
-          },
-          error: function(data) {
-            
-          }
-        });  
-    });
+            type: "POST",
+            url: "/share-to/friends",  
+            data: {'url': url},
+            success: function() {  
 
-    $('#post_fb').click(function() {
-        var publish = {
-            method: 'stream.publish',
-            message: 'has shared a link via moShare !',
-            picture : img,
-            link : currentShare,
-            name: title,
-            caption: '',
-            description: senderMess,
-            actions : { name : 'moShare it again!', link : newLink}
-        };
-
-             FB.api('/me/feed', 'POST', publish, function(response) {  
-                 if (!response || response.error) {
-                    console.log(response.error.message);
-                    $('#facebook_status_message').html('<p>Impossible to share with Facebook! An error occurred</p>').fadeIn('slow').removeClass('success').addClass('error');
-                  } else {
-                    $('#facebook_status_message').html('<p>Shared with Facebook!</p>').fadeIn('slow').removeClass('error').addClass('success');
-                  }
-             });
-    
+            },
+            error: function(data) {
+                //console.log(data.message);
+            }
+        });
     });
 });
